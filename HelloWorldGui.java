986df908efe2c5039a2b0a0a8b0e0503e978a378
Natasha S Love <debora@inbox.ru>
2018-08-22 20:54:22 -0500

    GUI file added

---
+++ HelloWorldGui.java
@@ -0,0 +1,7 @@
+import javax.swing.JOptionPane;
+public class HelloWorldGui {
+	public static void main(String [] args) {
+		// showMessageDialog needs 2 arguments - first is location or size of the window, second - text
+		JOptionPane.showMessageDialog(null, "Hello World!");
+	}
+}
