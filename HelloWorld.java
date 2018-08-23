57a8551c5b007475f43b7e894b6a233a8eec9e12
Natasha S Love <debora@inbox.ru>
2018-08-22 20:32:13 -0500

    Initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+class HelloWorld {
+	//array of as many strings as need - method
+	public static void main (String [] args) {
+		//easy way to print in java
+		System.out.println("Hello CIS2323");
+	}
+}
